4ed641820bf9f23f62c5c40b679ac5a5cd104830
sanjela <sanjela19@gmail.com>
2026-02-20 09:31:55 +0800

    updates

--- src/main/java/com/example/devops_demo/controller/DemoController.java
+++ src/main/java/com/example/devops_demo/controller/DemoController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
+//rest controller
 @RestController
 public class DemoController {
 
